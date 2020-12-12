d61f9845ea1019d7945fa509403a9bca6b023e91
vivek <vivek@viveks-MacBook-Pro.local>
2020-12-12 23:54:27 +0530

    formatting data for table view

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([])
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users').then(r => r.json()).then(d => {
+      d = d.map(({ name, username, email, id, phone, website, company, address }) => {
+        return ({
+          name, username, email, id, phone, website,
+          company: company.name, address: `${address.suite}, ${address.street}, ${address.city}`
+        })
+      })
+      setData(d)
+    })
+  }, [])
+  console.log(data);
   return (
     <div className="App">
       <header className="App-header">
 
-       
+
       </header>
     </div>
   );
