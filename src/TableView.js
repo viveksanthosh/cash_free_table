793e5d598368f0ff90b1d2f9600544d7027c99d5
vivek <vivek@viveks-MacBook-Pro.local>
2020-12-13 12:51:18 +0530

    tmp

--- src/TableView.js
+++ src/TableView.js
@@ -1,19 +1,64 @@
+import { useEffect, useState } from 'react'
 import './TableView.css'
 
 const TableView = ({ data, onDeleteClick,
     sortableHeaders, onSortClick }) => {
+
+    const [rows, setRows] = useState(null);
+    const [page, setPage] = useState(null);
+
+    useEffect(() => {
+        const paginationNeeded = (rows && rows < data.length)
+        if (!paginationNeeded) {
+            setPage(null); return;
+        }
+        else if (paginationNeeded && page === null) {
+            setPage(1); return;
+        }
+
+    }, [data, rows])
+
+    console.log({ data });
+
     if (!data || data.length === 0)
         return null
-    const headers = Object.keys(data[0])
-    return <table className='table'>
-        <thead>
-            <Header onSortClick={onSortClick} sortableHeaders={sortableHeaders} headers={headers} />
-        </thead>
-        <tbody>
-            {data.map(d => <Body onDeleteClick={onDeleteClick} key={d.id} headers={headers} values={d} />)}
-        </tbody>
 
-    </table>
+
+    const headers = Object.keys(data[0])
+
+    let displayData = data;
+    if (page) {
+        displayData = data.slice(page * rows, (page + 1) * rows)
+    }
+    console.log({ displayData });
+
+    return <section className='tableView'>
+
+        <label className='numberOrRows'>Number of rows to display
+            <input />
+        </label>
+
+        <table className='table'>
+            <thead>
+                <Header onSortClick={onSortClick}
+                    sortableHeaders={sortableHeaders}
+                    headers={headers} />
+            </thead>
+            <tbody>
+                {displayData.map(d => <Body
+                    onDeleteClick={onDeleteClick}
+                    key={d.id}
+                    headers={headers}
+                    values={d} />)}
+            </tbody>
+        </table>
+
+        {page && <div className='pagination'>
+            <span className={`${page === 0 ? 'disabled' : ''} pagination-button`}>Prev</span>
+            <span className={`pagination-number`}>2</span>
+            <span className={`${page === 0} pagination-button`}>Next</span>
+        </div>}
+    </section>
 }
 
 const Header = ({ headers, sortableHeaders, onSortClick }) => {
@@ -31,8 +76,8 @@ const Header = ({ headers, sortableHeaders, onSortClick }) => {
             key={h}
         >{h}
             <span className='arrowBox'>
-                {showArrow('down', sortableHeaders[h]) && <i class="arrow down"></i>}
-                {showArrow('up', sortableHeaders[h]) && <i class="arrow up"></i>}
+                {showArrow('down', sortableHeaders[h]) && <i className="arrow down"></i>}
+                {showArrow('up', sortableHeaders[h]) && <i className="arrow up"></i>}
             </span></th>)}
         <th>Actions</th>
     </tr>
